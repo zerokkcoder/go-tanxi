4b4dc5f776c7394ebb3fb7f5370aa39f576749b9
huanggz <918031078@qq.com>
2022-08-08 10:29:13 +0800

    添加路由

--- main.go
+++ main.go
@@ -23,6 +23,19 @@ func notFound(c *gin.Context) {
 		"<p>如有疑惑，请联系我们。</p>")
 }
 
+func articlesShow(c *gin.Context) {
+	id := c.Param("id")
+	c.String(http.StatusOK, "文章 ID: "+id)
+}
+
+func articlesIndex(c *gin.Context) {
+	c.String(http.StatusOK, "访问文章列表")
+}
+
+func articlesStore(c *gin.Context) {
+	c.String(http.StatusOK, "创建新的文章")
+}
+
 func main() {
 	// 1. 创建路由
 	r := gin.New()
@@ -30,7 +43,12 @@ func main() {
 	// gin.Context，封装了request和response
 	r.GET("/", index)
 	r.GET("/about", about)
+	r.GET("/articles/:id", articlesShow)
+	r.GET("/articles", articlesIndex)
+	r.POST("/articles", articlesStore)
+	// 自定义 404 页面
 	r.NoRoute(notFound)
+
 	// 3. 监听端口，默认在3000
 	// Run("里面不指定端口号默认为3000")
 	r.Run(":3000")
