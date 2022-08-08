a59c2b7059e9322ece20a64c1c6f554503d48721
huanggz <918031078@qq.com>
2022-08-08 13:24:32 +0800

    about 测试

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -7,17 +7,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHomePage(t *testing.T) {
-	baseURL := "http://localhost:3000"
+var baseURL = "http://localhost:3000"
 
-	// 1. 请求 —— 模拟用户访问浏览器
-	var (
-		resp *http.Response
-		err  error
-	)
+// 请求 —— 模拟用户访问浏览器
+var (
+	resp *http.Response
+	err  error
+)
+
+func TestHomePage(t *testing.T) {
+
 	resp, err = http.Get(baseURL + "/")
 
-	// 2. 检测 —— 是否无错误且 200
+	// 检测 —— 是否无错误且 200
+	assert.NoError(t, err, "有错误发生，err 不为空")
+	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
+}
+
+func TestAboutPage(t *testing.T) {
+
+	resp, err = http.Get(baseURL + "/about")
+
+	// 检测 —— 是否无错误且 200
 	assert.NoError(t, err, "有错误发生，err 不为空")
 	assert.Equal(t, 200, resp.StatusCode, "应返回状态码 200")
 }
