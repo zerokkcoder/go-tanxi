31a41de1a0801f06d61d614ca4c040b4a84eaec2
huanggz <918031078@qq.com>
2022-08-08 16:41:27 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"database/sql"
 	"go-tanxi/app/http/middlewares"
 	"go-tanxi/bootstrap"
 	"go-tanxi/pkg/logger"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
-
-// 1. 创建路由
-var router = mux.NewRouter()
-var db *sql.DB
 
 func main() {
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
 	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 	logger.LogError(err)
