dc099ab01907a2bd301c41819aa818beb7aa8361
huanggz <918031078@qq.com>
2022-08-08 12:34:21 +0800

    链接数据库

--- main.go
+++ main.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"text/template"
+	"time"
 	"unicode/utf8"
 
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
+
+// 1. 创建路由
+var router = mux.NewRouter()
+var db *sql.DB
+
+func initDB() {
+
+	var err error
+	config := mysql.Config{
+		User:                 "root",
+		Passwd:               "",
+		Addr:                 "127.0.0.1:3306",
+		Net:                  "tcp",
+		DBName:               "go_tanxi",
+		AllowNativePasswords: true,
+	}
+
+	// 准备数据库连接池
+	db, err = sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// 尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -132,10 +173,9 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
-// 1. 创建路由
-var router = mux.NewRouter()
-
 func main() {
+	initDB()
+
 	// 2. 绑定路由规则，执行的函数
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
