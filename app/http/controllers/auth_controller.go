162aca76b6075221f74515591a4c9e24139568f7
huanggz <918031078@qq.com>
2022-08-09 15:17:36 +0800

    创建用户

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"fmt"
+	"go-tanxi/app/models/user"
 	"go-tanxi/pkg/view"
 	"net/http"
 )
@@ -14,7 +16,26 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 	view.RenderSimple(w, view.D{}, "auth.register")
 }
 
-// DoRegister 注册
+// DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
+	// 0. 初始化变量
+	name := r.PostFormValue("name")
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+	// 1. 表单验证
 
+	// 2. 验证通过 —— 入库，并跳转到首页
+	_user := user.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+	}
+	_user.Create()
+	if _user.ID > 0 {
+		fmt.Fprint(w, "插入成功, ID 为: "+_user.GetStringID())
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "创建用户失败，请联系管理员")
+	}
+	// 3. 表单不通过 —— 重新显示表单
 }
