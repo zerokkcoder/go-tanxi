57990a90f52163f367ab7049126e19c921596eb7
huanggz <918031078@qq.com>
2022-08-10 14:57:52 +0800

    关联文章和分类

--- app/http/controllers/categories_controller.go
+++ app/http/controllers/categories_controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"go-tanxi/app/models/article"
 	"go-tanxi/app/models/category"
 	"go-tanxi/app/requests"
 	"go-tanxi/pkg/flash"
@@ -50,6 +51,22 @@ func (*CategoriesController) Store(w http.ResponseWriter, r *http.Request) {
 }
 
 // Show 显示分类下的文章列表
-func (*CategoriesController) Show(w http.ResponseWriter, r *http.Request) {
+func (cc *CategoriesController) Show(w http.ResponseWriter, r *http.Request) {
+	// 1. 获取 URL 参数
+	id := route.GetRouteVariable("id", r)
 
+	// 2. 读取对应的文章数据
+	_category, err := category.Get(id)
+
+	// 3. 获取结果集
+	articles, pagerData, err := article.GetByCategoryID(_category.GetStringID(), r, 2)
+	if err != nil {
+		cc.ResponseForSQLError(w, err)
+	} else {
+		// 4. 读取成功，显示文章
+		view.Render(w, view.D{
+			"Articles":  articles,
+			"PagerData": pagerData,
+		}, "articles.index", "articles._article_meta")
+	}
 }
